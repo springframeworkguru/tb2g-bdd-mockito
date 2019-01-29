ff9c7ba58026ab006a53704209e5f1c49bbd9ffc
John Thompson <john@springframework.guru>
2019-01-29 17:14:58 -0500

    adding controller test example

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,57 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class OwnerControllerTest {
+
+    private static final String OWNERS_CREATE_OR_UPDATE_OWNER_FORM = "owners/createOrUpdateOwnerForm";
+    private static final String REDIRECT_OWNERS_5 = "redirect:/owners/5";
+
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController controller;
+
+    @Mock
+    BindingResult bindingResult;
+
+    @Test
+    void processCreationFormHasErrors() {
+        //given
+        Owner owner = new Owner(1l, "Jim", "Bob");
+        given(bindingResult.hasErrors()).willReturn(true);
+
+        //when
+        String viewName = controller.processCreationForm(owner, bindingResult);
+
+        //then
+        assertThat(viewName).isEqualToIgnoringCase(OWNERS_CREATE_OR_UPDATE_OWNER_FORM);
+    }
+
+    @Test
+    void processCreationFormNoErrors() {
+        //given
+        Owner owner = new Owner(5l, "Jim", "Bob");
+        given(bindingResult.hasErrors()).willReturn(false);
+        given(ownerService.save(any())).willReturn(owner);
+
+        //when
+        String viewName = controller.processCreationForm(owner, bindingResult);
+
+        //then
+        assertThat(viewName).isEqualToIgnoringCase(REDIRECT_OWNERS_5);
+    }
+}
